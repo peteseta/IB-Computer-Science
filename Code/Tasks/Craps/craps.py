e42d1e638701bc89d290622cefefc71844188c92
peteseta <petesetabandhu@gmail.com>
2022-10-16 14:11:15 +0700

    refactoring

--- Code/Tasks/Craps/craps.py
+++ Code/Tasks/Craps/craps.py
@@ -10,7 +10,6 @@ def roll_dice():
 def end_screen_frame(global_root, source):
     # destroys the preceding frame; either the point frame or the come out roll frame depending when the user wins/loses
     source.destroy()
-    print("game over")
 
     # handles quitting the game (if they don't want to play again)
     def quit_game():
@@ -57,40 +56,36 @@ def point_frame(global_root, come_out_roll, point):
         if dice == 7:
             if bet == 0:
                 # win
+                result_text_var.set("You win!")
                 return 1
             else:
                 # lose
+                result_text_var.set("You lose!")
                 return 0
         elif dice == target_point:
             if bet == 0:
                 # lose
+                result_text_var.set("You lose!")
                 return 0
             else:
                 # win
+                result_text_var.set("You win!")
                 return 1
         elif bet == 1:
             # need to get point
+            result_text_var.set(
+                "Keep going. You have to roll a " + str(target_point) + "!"
+            )
             return 2
         elif bet == 0:
             # need to get a 7
+            result_text_var.set("Keep going. You have to roll a 7!")
             return 3
 
     # based on the result of the roll, update the result text and prompt the user to roll again or go to the end screen
     def point_roll():
         dice = roll_dice()
         result = calculate_point_roll(dice)
-
-        # text to show to the user based on result
-        result_text = (
-            "You win!"
-            if result == 1
-            else "You lose!"
-            if result == 0
-            else "Keep going. You have to roll a " + str(target_point) + "!"
-            if result == 2
-            else "Keep going. You have to roll a 7!"
-        )
-        result_text_var.set(result_text)
 
         # replacing the title with the number the player rolls
         point_title.destroy()
@@ -131,62 +126,61 @@ def come_out_roll_frame(global_root, make_bet):
     make_bet.destroy()
 
     # init label to prompt the user to roll. will later update with what was rolled.
+    rolled_text_var = StringVar()
+    rolled_text_var.set("Roll the dice!")
+
+    # init label to show the result of the roll. will later update with the result of the roll.
     result_text_var = StringVar()
-    result_text_var.set("Roll the dice!")
+    result_text_var.set("")
 
     # calculate result of the come out roll based on bet type and roll
     def calculate_first_roll(dice):
         if bet == 1:
             if dice == 7 or dice == 11:
                 # win
+                result_text_var.set("You win!")
                 return 1
             elif dice == 2 or dice == 3 or dice == 12:
                 # lose
+                result_text_var.set("You lose!")
                 return 0
             else:
                 # point
+                result_text_var.set("Point!")
                 return 3
         elif bet == 0:
             if dice == 2 or dice == 3:
                 # win
+                result_text_var.set("You win!")
                 return 1
             elif dice == 7 or dice == 11:
                 # lose
+                result_text_var.set("You lose!")
                 return 0
             elif dice == 12:
                 # tie
+                result_text_var.set("Tie!")
                 return 2
             else:
                 # point
+                result_text_var.set("Point!")
                 return 3
 
     # first roll of dice, on the initial press of the button. the dice is rolled and the result is shown to the user.
     def first_roll():
         # roll the dice and show the result
         dice = roll_dice()
-        result_text_var.set("You rolled a " + str(dice) + "!")
+        rolled_text_var.set("You rolled a " + str(dice) + "!")
 
         # calculate result of the roll
         result = calculate_first_roll(dice)
-
-        # description of result for label and console
-        result_text = (
-            "You win!"
-            if result == 1
-            else "You lose!"
-            if result == 0
-            else "Tie!"
-            if result == 2
-            else "Point!"
-        )
-        print(result_text)
 
         # destroy the title and button as there is only one first roll.
         come_out_roll_button.destroy()
         come_out_roll_title.destroy()
 
         # create a new label to show the result with description
-        come_out_roll_result = Label(come_out_roll, text=result_text)
+        come_out_roll_result = Label(come_out_roll, textvariable=result_text_var)
         come_out_roll_result.grid(row=3, column=2)
 
         # prompt the user to go to the next frame:
@@ -213,7 +207,7 @@ def come_out_roll_frame(global_root, make_bet):
     )
     come_out_roll_button.grid(row=1, column=2)
 
-    come_out_roll_label = Label(come_out_roll, textvariable=result_text_var)
+    come_out_roll_label = Label(come_out_roll, textvariable=rolled_text_var)
     come_out_roll_label.grid(row=2, column=2)
 
 
@@ -226,14 +220,14 @@ def make_bet_frame(global_root, source):
     def pass_line(global_root, make_bet):
         global bet
         bet = 1
-        print("pass set: " + str(bet))
+        print("pass line set")
         come_out_roll_frame(global_root, make_bet)
 
     # handles making the bet - No Pass Line
     def no_pass_line(global_root, make_bet):
         global bet
         bet = 0
-        print("no pass set: " + str(bet))
+        print("no pass line set")
         come_out_roll_frame(global_root, make_bet)
 
     # create frame for bet placing screen
