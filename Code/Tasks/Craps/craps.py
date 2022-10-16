998292a118ad9b29cce47f83b9a2fbca07b2716c
peteseta <petesetabandhu@gmail.com>
2022-10-16 13:02:22 +0700

    reformatting

--- Code/Tasks/Craps/craps.py
+++ Code/Tasks/Craps/craps.py
@@ -1,10 +1,8 @@
 import random
 from tkinter import Button, Frame, Label, StringVar, Tk
 
-
 # returns sum of two random integers 1-6 inclusive (2 dice roll)
 def roll_dice():
-
     return random.randint(1, 6) + random.randint(1, 6)
 
 
